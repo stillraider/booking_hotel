70108d38e3b6eef56d87beeabc32b8d7793f76a2
stillraider <61648212+stillraider@users.noreply.github.com>
2020-12-01 14:01:41 +0500

    without gulp files

--- #src/scripts/utility.js
+++
@@ -1,7 +0,0 @@
-function matchesSelector(el, selector) {
-	var p = Element.prototype;
-	var f = p.matches || p.webkitMatchesSelector || p.mozMatchesSelector || p.msMatchesSelector || function(s) {
-		return [].indexOf.call(document.querySelectorAll(s), this) !== -1;
-	};
-	return f.call(el, selector);
-}
